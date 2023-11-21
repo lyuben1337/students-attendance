d51fbf31594eaf7a4fb0f29f20a61d58e2fb989b
lyuben1337 <lyuben1337@gmail.com>
2023-11-21 14:13:27 +0100

    Initial Commit

---
+++ src/test/java/com/example/studentattendance/StudentAttendanceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.studentattendance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentAttendanceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
